74be3229cfa251d0209067ae771363f5bd5fc3c7
Adley Rodrigues <51420674+AdleyRodrigues@users.noreply.github.com>
2024-01-09 23:58:42 -0300

    Commit da paticipate e about

--- Projeto-Extensionista-WEB/src/pages/about/About.tsx
+++ Projeto-Extensionista-WEB/src/pages/about/About.tsx
@@ -1,11 +1,35 @@
+import { Box, Typography } from "@mui/material";
 import { MenuHeader } from "../../components/MenuHeader/MenuHeader";
 
 const About = () => {
   return (
-    <div>
+    <Box className="box-main">
       <MenuHeader />
-      <h1>About</h1>
-    </div>
+      <Box className="content-container">
+        <Typography variant="h4" className="content-title">
+          Projeto RecicLEIA
+        </Typography>
+        <br />
+        <Typography variant="body1" className="content-text">
+          <strong>Apoios -</strong>
+          <br />
+          ---
+        </Typography>
+        <br />
+        <Typography variant="body1" className="content-text">
+          <strong>Criadores -</strong>
+          <br />
+          ---
+        </Typography>
+        <br />
+        <Typography variant="body1" className="content-text">
+          <strong>
+            Dúvidas? Contate sua escola ou nos envie um email:
+            recicleia@sac.com.br -
+          </strong>
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
--- Projeto-Extensionista-WEB/src/pages/participate/Participate.tsx
+++ Projeto-Extensionista-WEB/src/pages/participate/Participate.tsx
@@ -1,11 +1,40 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
 import { MenuHeader } from "../../components/MenuHeader/MenuHeader";
+import "./Participate.css";
 
-const Participate = () => {
+export const Participate: React.FC = () => {
   return (
-    <div>
+    <Box className="box-main">
       <MenuHeader />
-      <h1>Participate</h1>
-    </div>
+      <Box className="content-container">
+        <Typography variant="h4" className="content-title">
+          Como participar?
+        </Typography>
+        <br/>
+        <Typography variant="body1" className="content-text">
+          <strong>1º -</strong> Faça seu cadastro, não esqueça de informar seus
+          dados corretamente e chame um adulto para auxiliar;
+        </Typography>
+        <br/>
+        <Typography variant="body1" className="content-text">
+          <strong>2º -</strong> Após terminar o cadastro e efetuar o login, é
+          hora de escolher um livro na biblioteca online de acordo com a
+          quantidade de itens recicláveis que você juntou/ irá juntar;
+        </Typography>
+        <br/>
+        <Typography variant="body1" className="content-text">
+          <strong>3º -</strong> Já juntou todos eles? Ótimo, agora você só
+          precisa levar os itens até a escola e fazer a troca informando seus
+          dados na secretaria.
+        </Typography>
+        <br/>
+        <Typography variant="body2" className="reminder-text">
+          Lembre-se sempre de verificar o ranking da sua turma e participar mais
+          vezes para concorrer a uma viagem com seus colegas!
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
